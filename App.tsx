ed4151be48a26de34c2846438de782f55303eda9
Guilherme Natal <guilhermenatal47@gmail.com>
2023-10-30 17:29:31 -0300

    Added redux and new password page.

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MenuButton from './src/Components/Menu/Button';
 import Login from './src/Screens/Auth/Login';
+import NewPassword from './src/Screens/Auth/NewPassword';
 import Sign from './src/Screens/Auth/Signup';
 
 export default function App() {
   return (
     <View className="flex-1">
-      <Sign />
+      <NewPassword />
     </View>
   );
 }
