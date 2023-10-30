61991ff53f8126cdf57b4daae0530cd125f89191
Guilherme Natal <guilhermenatal47@gmail.com>
2023-10-30 18:08:04 -0300

    Adding the API and requests

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MenuButton from './src/Components/Menu/Button';
-import Login from './src/Screens/Auth/Login';
-import NewPassword from './src/Screens/Auth/NewPassword';
-import Sign from './src/Screens/Auth/Signup';
+import { View } from 'react-native';
+import RecoverPassword from './src/Screens/Auth/RecoverPassword';
 
 export default function App() {
   return (
     <View className="flex-1">
-      <NewPassword />
+      <RecoverPassword />
     </View>
   );
 }
---
+++ src/Api/index.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+// import { getItemAsync, setItemAsync } from "expo-secure-store";
+
+export const Axios_API = axios.create({ baseURL: process.env.EXPO_PUBLIC_API_URL });
+console.log("Connecting to:", process.env.EXPO_PUBLIC_API_URL);
+
+export const setToken = async (token: string, refreshToken: string) => {
+  console.log(`Token: ${token} | RefreshToken: ${refreshToken}`);
+  Axios_API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+//   await setItemAsync("token", token);
+//   await setItemAsync("refreshToken", refreshToken);
+};
+
+export const loadToken = async (): Promise<boolean> => {
+//   const token = await getItemAsync("token");
+
+//   if (token) {
+//     Axios_API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+//     return true;
+//   }
+  return false;
+}
