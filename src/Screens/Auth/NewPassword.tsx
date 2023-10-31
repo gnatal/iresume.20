8e3459c62e6c0eab3948e81ddb0c2b4e42bf1747
wrighttzu <wrighttzu@gmail.com>
2023-10-31 12:35:56 -0300

    new pass with token

--- src/Screens/Auth/NewPassword.tsx
+++ src/Screens/Auth/NewPassword.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Controller, useForm } from "react-hook-form";
 import { Text, View, TouchableOpacity, Image, Platform, TextInput, ScrollView } from "react-native";
+import { newPassWithToken } from "../../Services/newPasswordWithToken";
+import * as yup from "yup";
+
+const newPasswordSchema = yup.object().shape({
+  token:yup.string().required("Token required"),
+  password: yup.string().required().min(8),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "passwords must match"),
+});
 
 function NewPassword({ navigation }: any) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(newPasswordSchema),
+    defaultValues: {
+      password: "",
+      passwordConfirmation: "",
+      token: "",
+    },
+  });
+
+  const handleNewPassword = async ({ token, password }: {token: string, password: string}) => {
+    try {
+      const { status, data } = await newPassWithToken(token, password);
+      console.log({ status, data });
+      if (status === 200) {
+        navigation.navigate("Login");
+      }
+    } catch (error) {
+      console.log(
+        `NewPassWithToken.handleLogin: Exception=${JSON.stringify(error)}`
+      );
+    }
+  };
+
   return (
     <ScrollView >
       <View className="flex justify-start items-center w-screen h-screen bg-[#FFFFFF]">
@@ -10,22 +50,91 @@ function NewPassword({ navigation }: any) {
         </View>
         <View className="bg-[#F5F5F5] w-4/5 pb-10 mt-10 border-2 items-center border-solid  rounded-xl border-[#9FC0C7] shadow-xl shadow-black">
           <Text className="w-4/5 my-8 text-center">Informe uma nova senha</Text>
-          <TextInput
-            className={`h-10 w-4/5 my-2 text-black bg-[#F0F0F0] rounded-lg ${
+          <View
+            className={`w-4/5 text-black bg-[#F0F0F0] rounded-lg ${
               Platform.OS === "ios" ? "shadow-sm" : "shadow-lg"
-            } shadow-black px-2`}
-            placeholder="New Password"
-          />
-          <TextInput
-            className={`h-10 w-4/5 my-2 text-black bg-[#F0F0F0] rounded-lg ${
+            } shadow-black px-2 justify-center h-12 mb-2 mt-2`}
+          >
+            <Controller
+              control={control}
+              name="password"
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  className="h-10 text-black"
+                  placeholder="Senha"
+                  secureTextEntry={true}
+                  placeholderTextColor="#9E9E9E"
+                  onChangeText={onChange}
+                />
+              )}
+            />
+          </View>
+          {errors.password && (
+            <Text className="text-[#FF5252] mb-2">
+              {errors?.password?.message}
+            </Text>
+          )}
+          <View
+            className={`w-4/5 text-black bg-[#F0F0F0] rounded-lg ${
               Platform.OS === "ios" ? "shadow-sm" : "shadow-lg"
-            } shadow-black px-2`}
-            placeholder="Confirm Password"
-          />
+            } shadow-black px-2 justify-center h-12 mb-2 mt-2`}
+          >
+            <Controller
+              control={control}
+              name="passwordConfirmation"
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  className="h-10 text-black"
+                  placeholder="Confirme sua senha"
+                  secureTextEntry={true}
+                  placeholderTextColor="#9E9E9E"
+                  onChangeText={onChange}
+                />
+              )}
+            />
+          </View>
+          {errors.passwordConfirmation && (
+            <Text className="text-[#FF5252] mb-2">
+              {errors?.passwordConfirmation?.message}
+            </Text>
+          )}
+          <View
+            className={`w-4/5 text-black bg-[#F0F0F0] rounded-lg ${
+              Platform.OS === "ios" ? "shadow-sm" : "shadow-lg"
+            } shadow-black px-2 justify-center h-12 mb-2 mt-2`}
+          >
+            <Controller
+              control={control}
+              name="token"
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  className="h-10 text-black"
+                  placeholder="Token"
+                  placeholderTextColor="#9E9E9E"
+                  onChangeText={onChange}
+                />
+              )}
+            />
+          </View>
+          {errors.token && (
+            <Text className="text-[#FF5252] mb-2">
+              {errors?.token?.message}
+            </Text>
+          )}
           <TouchableOpacity
-            className={`h-10 w-4/5 mt-4 bg-[#42A5F5] justify-center rounded-lg items-center ${
+            onPress={handleSubmit(handleNewPassword)}
+            className={`h-12 w-4/5 bg-[#42A5F5] justify-center rounded-lg items-center ${
               Platform.OS === "ios" ? "shadow-sm" : "shadow-lg"
-            } shadow-black`}
+            } shadow-black mt-8 mb-0`}
           >
             <Text className="text-white">Enviar</Text>
           </TouchableOpacity>
