0218a022138414efcd4a958abd862574d92386e4
Estevam Cardoso Lopes da Silva <estevam19.15@gmail.com>
2023-11-19 20:42:09 -0300

    Profile: Add hero to profile View just like the others

--- src/Components/Profile/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo.tsx
@@ -6,9 +6,8 @@ import { FadePanel } from "../Basics/FadePanel";
 
 const Box = ({ className = "", ...props }) => (
   <View
-    className={`flex text-center justify-center items-center rounded-lg ${
-      Platform.OS === "ios" ? "shadow-sm" : "shadow-lg"
-    } shadow-black px-2 bg-[#F0F0F0] ${className}`}
+    className={`flex text-center justify-center items-center rounded-lg ${Platform.OS === "ios" ? "shadow-sm" : "shadow-lg"
+      } shadow-black px-2 bg-[#F0F0F0] ${className}`}
     {...props}
   />
 );
@@ -24,56 +23,57 @@ const ProfileInfo: React.FC<IProfileInfoProps> = ({
   navigation,
 }) => {
   return (
-    <View>
-      <View
-        className={`w-4/5 mt-10 p-1 items-center rounded-xl ${
-          Platform.OS === "ios" ? "shadow-sm" : "shadow-xl"
+    <View
+      className={`w-4/5 mt-10 items-center rounded-xl ${Platform.OS === "ios" ? "shadow-sm" : "shadow-xl"
         } shadow-black bg-[#F5F5F5]`}
-      >
-        {/* Inside BOX */}
-        <View className="flex flex-row mx-2">
-          <View className="basis-1/3 my-1 p-1 justify-center">
-            <Box className="basis-full m-1 h-40 px-0">
-              {/*  ${Platform.OS === 'ios' ? 'rounded-xl' : 'rounded-lg'} */}
-              <Image
-                className={`h-full w-full rounded-lg ${
-                  Platform.OS === "ios" ? "rounded-xl" : ""
+    >
+      {/* Inside BOX */}
+      <View className="w-[100%] py-2 bg-[#0D47A1] rounded-t-xl">
+        <TextFields className="w-full text-xl text-white text-center">
+          Informações Pessoais
+        </TextFields>
+      </View>
+      <View className="flex flex-row mx-2">
+        <View className="basis-1/3 my-1 p-1 justify-center">
+          <Box className="basis-full m-1 h-40 px-0">
+            {/*  ${Platform.OS === 'ios' ? 'rounded-xl' : 'rounded-lg'} */}
+            <Image
+              className={`h-full w-full rounded-lg ${Platform.OS === "ios" ? "rounded-xl" : ""
                 }`}
-                source={
-                  ProfileInfo?.photo != ""
-                    ? { uri: ProfileInfo?.photo }
-                    : require("../../../assets/default-profile-picture.png")
-                }
-              />
-            </Box>
-          </View>
-          <View className="basis-2/3 p-1 justify-center">
-            <Box className="basis-full m-1 h-8">
-              <TextFields>{ProfileInfo?.name}</TextFields>
-            </Box>
-            <Box className="basis-full m-1 h-8">
-              <TextFields>{ProfileInfo?.email}</TextFields>
-            </Box>
-            <Box className="basis-full m-1 h-8">
-              <TextFields>{ProfileInfo?.phone}</TextFields>
-            </Box>
-            <Box className="basis-full m-1 h-8">
-              <TextFields>{ProfileInfo?.address}</TextFields>
-            </Box>
-          </View>
+              source={
+                ProfileInfo?.photo != ""
+                  ? { uri: ProfileInfo?.photo }
+                  : require("../../../assets/default-profile-picture.png")
+              }
+            />
+          </Box>
         </View>
-        <View className="flex flex-row mx-2 mb-2">
-          <View className="basis-full p-1 justify-center">
-            <Box className="basis-full h-auto" style={{ minHeight: 32 }}>
-              <TextFields>{ProfileInfo?.description}</TextFields>
-            </Box>
-          </View>
+        <View className="basis-2/3 p-1 justify-center">
+          <Box className="basis-full m-1 h-8">
+            <TextFields>{ProfileInfo?.name}</TextFields>
+          </Box>
+          <Box className="basis-full m-1 h-8">
+            <TextFields>{ProfileInfo?.email}</TextFields>
+          </Box>
+          <Box className="basis-full m-1 h-8">
+            <TextFields>{ProfileInfo?.phone}</TextFields>
+          </Box>
+          <Box className="basis-full m-1 h-8">
+            <TextFields>{ProfileInfo?.address}</TextFields>
+          </Box>
+        </View>
+      </View>
+      <View className="flex flex-row mx-2 mb-2">
+        <View className="basis-full p-1 justify-center">
+          <Box className="basis-full h-auto" style={{ minHeight: 32 }}>
+            <TextFields>{ProfileInfo?.description}</TextFields>
+          </Box>
         </View>
       </View>
       <FadePanel visible={Edit}>
         <EditButton
           disabled={!Edit}
-          className="absolute bottom-[-10] right-[-10]"
+          className="absolute bottom-[-10] right-[-55%]"
           onPress={() => {
             navigation.navigate("EditProfileInfo");
           }}
